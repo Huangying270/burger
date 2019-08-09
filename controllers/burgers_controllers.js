1d1a08585ba9c947129765dcdae5f4b6176c45b9
Ying Huang <huangying270@gmail.com>
2019-08-08 20:57:57 -0400

    update connection orm burgers and controller js

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,8 +3,43 @@ var burgers = require("../models/burger.js");
 var router = express.Router();
 
 
-
-
+router.get("/burgers", function(req, res) {
+    burgers.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/burgers/create", function(req, res) {
+    burgers.insertOne([
+      "burger_name"
+    ], [
+      req.body.burger_name
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/burgers/update/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burgers.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 
 
