286ec6196cc9eadaedfca9bf20aa61493452f6b6
Ying Huang <huangying270@gmail.com>
2019-08-13 11:49:58 -0400

    edited controller and main

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,7 +3,7 @@ var burgers = require("../models/burger.js");
 var router = express.Router();
 
 
-router.get("/burgers", function(req, res) {
+router.get("/", function(req, res) {
     burgers.selectAll(function(data) {
       var hbsObject = {
         burgers: data
@@ -13,18 +13,18 @@ router.get("/burgers", function(req, res) {
     });
   });
   
-  router.post("/burgers/create", function(req, res) {
+  router.post("/burgers", function(req, res) {
     burgers.insertOne([
-      "burger_name"
+      "burger_name", "devoured"
     ], [
-      req.body.burger_name
+      req.body.burger_name, req.body.devoured
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
     });
   });
   
-  router.put("/burgers/update/:id", function(req, res) {
+  router.put("/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
